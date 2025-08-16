99df9e6f3960839249491281404211c4c71149d5
githubotoro <yupuday.git@gmail.com>
2025-08-16 19:18:37 -0400

    feat(login): fix

--- hera-overlay/src/renderer/src/components/ProtectedRoute.tsx
+++ hera-overlay/src/renderer/src/components/ProtectedRoute.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAtom } from 'jotai';
 import { usePersistentAtom } from '@renderer/store/persistence';
+import { useQuery } from '@tanstack/react-query';
+import { api } from '@renderer/api';
+import { keepPreviousData } from '@tanstack/react-query';
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -9,6 +12,32 @@ interface ProtectedRouteProps {
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const [userToken, setUserToken] = usePersistentAtom('userToken', undefined);
+
+  const { data: userInfo, isLoading: isLoadingUserInfo } = useQuery({
+    queryKey: [
+      'user-info',
+      {
+        userToken
+      }
+    ],
+    queryFn: async () => {
+      return api.api
+        .userControllerGetUserInfo({
+          userToken: userToken ?? ''
+        })
+        .then((res) => res.data);
+    },
+    refetchInterval: 1 * 60 * 1000, // 1 minute
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+    placeholderData: keepPreviousData
+  });
+
+  useEffect(() => {
+    if (userToken && !userInfo && !isLoadingUserInfo) {
+      setUserToken(undefined);
+    }
+  }, [userInfo, isLoadingUserInfo, userToken]);
 
   if (!userToken) {
     return <Navigate to="/signin" replace />;
